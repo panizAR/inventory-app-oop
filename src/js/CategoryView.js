07c3b70b06e18a8d48128e01d08f6da8028ceb43
p.a.r <p.a.r.mygit@gmail.com>
2024-12-29 22:55:22 +0330

    class categoryview

--- src/js/CategoryView.js
+++ src/js/CategoryView.js
@@ -1,1 +1,24 @@
-export default class CategoryView {}
+import Storage from "./Storage.js";
+
+const title = document.querySelector("#category-title");
+const description = document.querySelector("#category-description");
+const addNewCategory = document.querySelector("#add-new-category");
+const btnCancel = document.querySelector(".btn-cancel");
+
+export default class CategoryView {
+  constructor() {
+    addNewCategory.addEventListener("click", (e) => this.addNewCategory(e));
+    this.Categoriees = [];
+  }
+  addNewCategory(e) {
+    e.preventDefault();
+    const title = title.value;
+    const description = description.value;
+    if (!title || !description) return;
+    Storage.saveCategory({ title, description });
+    this.Categoriees = Storage.getAllCategories();
+
+    // update DOM : Update select option
+    
+  }
+}
